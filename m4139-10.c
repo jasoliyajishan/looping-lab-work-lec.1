07f820c42fc02e304c09aa9d4d9feff48645fd5b
jasoliyajishan <132878572+jasoliyajishan@users.noreply.github.com>
2023-05-19 09:54:50 +0530

    Add files via upload

---
+++ m4139-10.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+main()
+{
+int n,i=1;
+printf("enter value n=");
+scanf("%d",&n);
+while(i<=10)
+{
+	printf("%d*%d=%d\n",n,i,n*i);
+	i++;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
+	
+}
+	
+	
+	
+	
