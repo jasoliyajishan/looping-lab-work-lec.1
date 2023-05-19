43e4d31749ded1c04fb304a87e7b6dcc5ac3dc31
jasoliyajishan <132878572+jasoliyajishan@users.noreply.github.com>
2023-05-19 12:59:52 +0530

    m4139-10.c

--- m4139-10.c
+++ m4139-10.c
@@ -1,17 +1,13 @@
-#include <stdio.h>
+#include<stdio.h>
+main()
+{
+int n,i=1;
+printf("enter value n=");
+scanf("%d",&n);
+while(i<=10)
+{
+	printf("%d*%d=%d\n",n,i,n*i);
+	i++;
+}
 
-int main()
- {
-    int i=1,n;
-    printf("enter a number");
-    scanf("%d",&n);
-
-    while (i <= n) {
-    	
-    	
-        printf("%d\n", i);
-        i++;
-    }
-
-    return 0;
 }
