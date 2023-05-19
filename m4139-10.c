2c4e82f69cb7a35193bb14160cf39eddde092873
jasoliyajishan <132878572+jasoliyajishan@users.noreply.github.com>
2023-05-19 12:21:47 +0530

    m4139-10.c

--- m4139-10.c
+++ m4139-10.c
@@ -1,17 +1,17 @@
-#include<stdio.h>
+#include <stdio.h>
+
 main()
-{
-int n,i=1;
-printf("enter value n=");
-scanf("%d",&n);
-while(i<=10)
-{
-	printf("%d*%d=%d\n",n,i,n*i);
-	i++;
+ {
+   int n,i,factorial=1;
+    
+    printf("enter a number");
+    scanf("%d",&n);
+
+    while(n>=1)
+    {
+        factorial=factorial*n;
+        n--;
+    }
+    printf("The Factorial of  is : %d",factorial);
+return 0;
 }
-	
-}
-	
-	
-	
-	
